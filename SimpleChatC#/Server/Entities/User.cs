81d2f262c48bd9b8bb87def991d184335c6d3489
JTKlemmensen <jonastklemmensen@gmail.com>
2019-05-13 01:13:34 +0200

    Added user entity and interface for user repository

---
+++ SimpleChatC#/Server/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+
+        //TODO Set should hash the password and set the salt
+        public string Password { get; set; }
+        public string Salt { get; private set; }
+    }
+}
---
+++ SimpleChatC#/Server/Repositories/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Server.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Repositories
+{
+    public interface IUserRepository
+    {
+        List<User> Users { get; }
+        User Login(string username, string password);
+        User SaveUser(User user);
+        void DeleteUser(User user);
+    }
+}
