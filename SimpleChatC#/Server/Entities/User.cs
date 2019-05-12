5d84ac63e25e09778f408b3fc3ff9825cbee0255
JTKlemmensen <jonastklemmensen@gmail.com>
2019-05-13 01:23:04 +0200

    Hashes passwords when set

--- SimpleChatC#/Server/Entities/User.cs
+++ SimpleChatC#/Server/Entities/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DevOne.Security.Cryptography.BCrypt;
 
 namespace Server.Entities
 {
@@ -9,8 +10,15 @@ namespace Server.Entities
         public int Id { get; set; }
         public string Username { get; set; }
 
-        //TODO Set should hash the password and set the salt
-        public string Password { get; set; }
+        private string password;
+        public string Password {
+            get => password;
+            set
+            {
+                Salt = BCryptHelper.GenerateSalt();
+                password = BCryptHelper.HashPassword(value, Salt);
+            }
+        }
         public string Salt { get; private set; }
     }
 }
