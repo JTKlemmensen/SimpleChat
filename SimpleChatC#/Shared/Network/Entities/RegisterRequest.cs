864feb283a66d5f0b2781d893e6ade489f285644
JTKlemmensen <jonastklemmensen@gmail.com>
2019-05-13 23:51:34 +0200

    Added login+register requests

---
+++ SimpleChatC#/Shared/Network/Entities/RegisterRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Network.Entities
+{
+    public class RegisterRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
