6ccfc8e9072040fb214420655103d47a2a71670a
JTKlemmensen <jonastklemmensen@gmail.com>
2019-05-17 15:18:44 +0200

    Organized the entities that will be sent over the network

---
+++ SimpleChatC#/Shared/Network/Entities/ServerToClient/LoginSuccess.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Network.Entities
+{
+    public class LoginSuccess
+    {
+        public List<string> Users { get; set; }
+    }
+}
