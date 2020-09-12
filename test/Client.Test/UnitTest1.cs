6a25b89376562667e2c3ac3a43120e8658d23b5d
Jonas Klemmensen <jonastklemmensen@gmail.com>
2020-09-12 13:24:26 +0200

    added a basic unit test

--- test/Client.Test/UnitTest1.cs
+++ test/Client.Test/UnitTest1.cs
@@ -8,7 +8,11 @@ namespace Client.Test
         [Fact]
         public void Test1()
         {
+            var c = new Class1();
 
+            var result = c.Plus(1, 2);
+
+            Assert.Equal(3, result);
         }
     }
 }
